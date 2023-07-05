c292d819d412afc7a1429980c287a6dc79cf9f6d
AnushaPatelHS92 <138655337+AnushaPatelHS92@users.noreply.github.com>
2023-07-05 09:32:24 +0000

    add the changes1

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,9 +5,12 @@ set -e
 echo "executing $0 {"
 
 rm -rf hello1_webserver/
-git clone https://github.com/AnushaPatelHS92/hello1_src.git
+git clone https://github.com/AnushaPatelHS92/hello1_webserver.git
 cd hello1_webserver/
-cp 
+cp ../Index.html .
+git add --all
+git commit -m "$(date)"
+git push
 
 rm -rf hello1_webserver/
 echo "done $0 }"
