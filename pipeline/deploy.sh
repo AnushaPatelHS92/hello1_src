f5cb8f884ddc40e89efe074e7ab5ffba9f7cffcd
AnushaPatelHS92 <138655337+AnushaPatelHS92@users.noreply.github.com>
2023-07-05 09:23:54 +0000

    add changes

---
+++ pipeline/deploy.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env sh
+
+set -e
+
+echo "executing $0 {"
+
+rm -rf hello1_webserver/
+git clone https://github.com/AnushaPatelHS92/hello1_src.git
+cd hello1_webserver/
+cp 
+
+rm -rf hello1_webserver/
+echo "done $0 }"
