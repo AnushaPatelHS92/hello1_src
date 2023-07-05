e70f3f4edf5e0aa9b9ac351150adf04ff543d332
AnushaPatelHS92 <138655337+AnushaPatelHS92@users.noreply.github.com>
2023-07-05 10:11:50 +0000

    fixdeploy

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -7,6 +7,8 @@ echo "executing $0 {"
 rm -rf hello1_webserver/
 git clone "https:///$MACHINE_USER_TOKEN@github.com/AnushaPatelHS92/hello1_webserver.git"
 cd hello1_webserver/
+git config user.email "user@machine.com"
+git config user.name "machine_user"
 cp ../Index.html .
 git add --all
 git commit -m "$(date)" || true 
