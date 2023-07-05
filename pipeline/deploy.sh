1fb61a62d5fed11366233c2996f2f09fb9fd3410
AnushaPatelHS92 <138655337+AnushaPatelHS92@users.noreply.github.com>
2023-07-05 10:03:49 +0000

    add delpoy

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,7 +5,7 @@ set -e
 echo "executing $0 {"
 
 rm -rf hello1_webserver/
-git clone https://github.com/AnushaPatelHS92/hello1_webserver.git
+git clone "https:///$MACHINE_USER_TOKEN@github.com/AnushaPatelHS92/hello1_webserver.git"
 cd hello1_webserver/
 cp ../Index.html .
 git add --all
