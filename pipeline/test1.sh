683a8646ce22499e52898e9d4e7ea8b985231ce9
AnushaPatelHS92 <138655337+AnushaPatelHS92@users.noreply.github.com>
2023-07-05 09:10:44 +0000

    add new path

---
+++ pipeline/test1.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+set -e
+
+echo "hallo" ;
+exit 246
+
+cd gibtsnicht || true
+
+cd gibtsnicht  || echo "ging nicht" && echo "ging"
+
+if cd gibtnich ; then
+    echo ging
+else
+    echo ging nicht
+fi
+echo "gehtdoch"
